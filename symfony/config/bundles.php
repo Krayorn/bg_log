392176b9458fcf8abdb402a19207bb415918a6ae
Krayorn <nath.arki36@gmail.com>
2026-03-01 20:24:16 +0100

    twig config

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -16,7 +16,8 @@ return [
         'all' => true,
     ],
     Symfony\Bundle\TwigBundle\TwigBundle::class => [
-        'all' => true,
+        'dev' => true,
+        'test' => true,
     ],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => [
         'dev' => true,
